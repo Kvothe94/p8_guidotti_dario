cee28e909989f1cb36b80987d006ad0aba4ec808
Kvothe94 <guido9494@gmail.com>
2017-05-09 17:10:45 +0200

    Commit Imp 0.1

---
+++ Eclipse Project/src/Pattern.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+/**
+ * @author Guidotti Dario
+ *
+ */
+public abstract class Pattern {
+	
+	protected String patternVar1;
+	
+	public abstract String asString();
+	
+}
---
+++ Eclipse Project/src/QualitativePattern.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author Guidotti Dario
+ *
+ */
+public abstract class QualitativePattern extends Pattern {
+
+	/* (non-Javadoc)
+	 * @see Pattern#asString()
+	 */
+	@Override
+	public abstract String asString();
+
+}
